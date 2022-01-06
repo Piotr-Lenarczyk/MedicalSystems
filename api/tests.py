249b52e053e2da8a24caa36080c985c8a04e09ee
Piotr-Lenarczyk <piotr.lenarczyk@op.pl>
2022-01-06 17:19:47 +0100

    Added new test cases (2).

--- api/tests.py
+++ api/tests.py
@@ -88,6 +88,7 @@ class AccessTestCase(TestCase):
 class AdvancedModelTestCase(TestCase):
     # An advanced model is a model with at least one relation to another model
     def setUp(self):
+        # Prerequisites
         user1 = User.objects.create_user('User1', 'email1@email.com', 'password', is_doctor=True, is_patient=False)
         user2 = User.objects.create_user('User2', 'email2@email.com', 'password', is_doctor=False, is_patient=True)
         country = Country.objects.create(country_assigned='POL', currency='PLN')
@@ -138,3 +139,111 @@ class AdvancedModelTestCase(TestCase):
             self.assertTrue(True)
         else:
             self.assertTrue(False)
+
+
+# Check whether user has the permissions that allow access to a resource
+class PermissionTestCase(TestCase):
+    def setUp(self):
+        # Prerequisites
+        User.objects.create_superuser('admin', 'admin@admin.com', 'password')
+        user1 = User.objects.create_user('user1', 'email1@email.com', 'password', first_name='Doctor', last_name='One',
+                                         is_doctor=True, is_patient=False)
+        user2 = User.objects.create_user('user2', 'email2@email.com', 'password', first_name='Patient', last_name='One',
+                                         is_doctor=False, is_patient=True)
+
+        country = Country.objects.create(country_assigned='POL', currency='PLN')
+        user_profile1 = UserProfile.objects.create(user=user1, title='Doc', address='Add1', country=country,
+                                                   city='Warsaw', zip='00-000', hospital_ward='ER')
+        user_profile2 = UserProfile.objects.create(user=user2, title='Mr', address='Add2', country=country,
+                                                   city='Warsaw', zip='00-000', hospital_ward='ER')
+        doctor = Doctor.objects.create(doctor_id=1, email=user1, user=user_profile1)
+        patient = Patient.objects.create(patient_id=1, email=user2, user=user_profile2, date_of_admission='2021-12-01')
+        specialization = Specialization.objects.create(name='Neurology')
+        address = Address.objects.create(address_id=1, street='Street', house_number=10, apartment_number=20,
+                                         city='Opole', postal_code='11-111', state='Upper Silesia', country='POL')
+        Visit.objects.create(visit_id=1, visited_patient=patient, date=datetime.date.today(), time=datetime.time(),
+                             location=address, required_specialization=specialization, leading_doctor=doctor)
+        Result.objects.create(result_id=1, target_patient=patient, subject='Subject', description='This is a test.')
+
+    def testValidatePrerequisites(self):
+        """User has properly logged in"""
+        client = Client()
+
+        # Login as administration personnel
+        client.login(username='admin@admin.com', password='password')
+        response = client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+        # Login as medical personnel
+        client.login(username='email1@email.com', password='password')
+        response = client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+        # Login as patient
+        client.login(username='email1@email.com', password='password')
+        response = client.get('/api/')
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+    def testViewPermission(self):
+        """Permissions of user are successfully verified; proper view access level is granted"""
+        client = Client()
+
+        # Access view as administration personnel
+        client.login(username='admin@admin.com', password='password')
+        # List out all instances in a view
+        response = client.get('/api/users/')
+        self.assertEqual(response.status_code, 200)
+        # Retrieve a single instance in a view
+        response = client.get('/api/users/1/')
+        self.assertEqual(response.status_code, 200)
+        # Create a new instance in a view
+        response = client.post('/api/results/', {'result_id': 2, 'target_patient': 1, 'subject': 'Subject2',
+                                                 'description': 'Another test'})
+        self.assertEqual(response.status_code, 201)
+        # Partially update existing instance in a view
+        response = client.patch('/api/results/1/', {'description': 'Another TEST'}, 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Result.objects.get(result_id=1).description, 'Another TEST')
+
+        response = client.get('/api/doctors/')
+        self.assertEqual(response.status_code, 200)
+        response = client.get('/api/doctors/1/')
+        self.assertEqual(response.status_code, 200)
+        client.logout()
+
+        # Access view as medical personnel
+        client.login(username='email1@email.com', password='password')
+        # Access should be granted, returning a 200 OK status code
+        response = client.get('/api/patients/')
+        self.assertEqual(response.status_code, 200)
+        response = client.get('/api/patients/1/')
+        self.assertEqual(response.status_code, 200)
+        response = client.post('/api/results/', {'result_id': 3, 'target_patient': 1, 'subject': 'Subject3',
+                                                 'description': 'Yet another test'})
+        self.assertEqual(response.status_code, 201)
+        # Access should be denied, returning a 403 Forbidden status code
+        response = client.get('/api/users/')
+        self.assertEqual(response.status_code, 403)
+        response = client.get('/api/users/1/')
+        self.assertEqual(response.status_code, 403)
+        response = client.post('/api/specializations/', {'name': 'ER'})
+        self.assertEqual(response.status_code, 403)
+        client.logout()
+
+        # Access view as patient
+        client.login(username='email2@email.com', password='password')
+        # Access should be granted, returning a 200 OK status code
+        response = client.get('/api/results/1/')
+        self.assertEqual(response.status_code, 200)
+        # Access should be denied, returning a 403 Forbidden status code
+        response = client.get('/api/doctors/')
+        self.assertEqual(response.status_code, 403)
+        response = client.get('/api/doctors/1/')
+        self.assertEqual(response.status_code, 403)
+        response = client.post('/api/results/', {'result_id': 4, 'target_patient': 1, 'subject': 'Subject4',
+                                                 'description': 'Yet again, another test'})
+        self.assertEqual(response.status_code, 403)
+        client.logout()
