d832fd31d325cab4125a47cfc579e7d592d91392
Piotr-Lenarczyk <piotr.lenarczyk@op.pl>
2022-01-27 17:48:46 +0100

    Alter forms design

--- api/forms.py
+++ api/forms.py
@@ -2,33 +2,33 @@ from django import forms
 from api.models import *
 
 
-class PatientVisitForm(forms.ModelForm):
-    class Meta:
-        model = Visit
-        fields = ['visited_patient', 'date', 'time', 'location', 'required_specialization', 'leading_doctor']
-
-
 class RawPatientVisitForm(forms.Form):
-    visited_patient = forms.ModelChoiceField(queryset=Patient.objects.all())
+    visited_patient = forms.ModelChoiceField(queryset=Patient.objects.all(), label='Your ID')
     date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
     location = forms.ModelChoiceField(queryset=Address.objects.all())
     required_specialization = forms.ModelChoiceField(queryset=Specialization.objects.all())
-    leading_doctor = forms.ModelChoiceField(queryset=Doctor.objects.all())
+    leading_doctor = forms.ModelChoiceField(queryset=Doctor.objects.all(), label='Doctor ID')
 
 
 class RawDoctorRecommendationForm(forms.Form):
-    target_patient = forms.ModelChoiceField(queryset=Patient.objects.all())
-    subject = forms.CharField()
-    description = forms.CharField(widget=forms.Textarea())
-    to_discharge = forms.ModelChoiceField(queryset=Discharge.objects.all())
+    target_patient = forms.ModelChoiceField(queryset=Patient.objects.all(), label='Patient ID')
+    subject = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'What is it related to?',
+    }))
+    description = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Enter the recommendation'
+    }))
+    to_discharge = forms.ModelChoiceField(queryset=Discharge.objects.all(), label='Confirm patient ID')
 
 
 class RawDoctorMedicationForm(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter medication name',
+    }))
     dose_in_milligrams = forms.DecimalField()
     dosage_daily = forms.DecimalField()
-    to_prescription = forms.ModelChoiceField(queryset=Prescription.objects.all())
+    to_prescription = forms.ModelChoiceField(queryset=Prescription.objects.all(), label='Patient ID')
 
 
 class RawDoctorStateForm(forms.Form):
@@ -36,10 +36,12 @@ class RawDoctorStateForm(forms.Form):
     diastolic_blood_pressure = forms.DecimalField()
     heart_rate = forms.DecimalField()
     blood_oxygen_level = forms.DecimalField()
-    to_patient_state = forms.ModelChoiceField(queryset=PatientStates.objects.all())
+    to_patient_state = forms.ModelChoiceField(queryset=PatientStates.objects.all(), label='Patient ID')
 
 
 class RawDoctorIllnessForm(forms.Form):
-    patient_id = forms.ModelChoiceField(queryset=Patient.objects.all())
-    name = forms.CharField()
-    to_patient_illness = forms.ModelChoiceField(queryset=PatientIllnesses.objects.all())
+    patient_id = forms.ModelChoiceField(queryset=Patient.objects.all(), label='Patient ID')
+    name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter illness name',
+    }))
+    to_patient_illness = forms.ModelChoiceField(queryset=PatientIllnesses.objects.all(), label='Confirm patient ID')
--- api/tasks.py
+++ api/tasks.py
@@ -1,4 +1,4 @@
-from api.models import Country
+from api.models import *
 from api.serializers import request_currency_rate
 import datetime
 
@@ -8,3 +8,16 @@ def update_rates():
         country.rate = request_currency_rate(country.currency)
         country.last_update = datetime.datetime.now()
         country.save()
+
+
+def update_discharge():
+    patients = Patient.objects.all()
+    for patient in patients:
+        if Discharge.objects.filter(patient_id=patient.id).exists():
+            print("Patient found")
+        else:
+            Discharge.objects.create(patient_id=patient.id)
+            discharge = Discharge.objects.get(patient_id=patient.id)
+            Prescription.objects.create(patient_id=patient, to_discharge=discharge)
+            PatientStates.objects.create(patient_id=patient.id)
+            PatientIllnesses.objects.create(patient_id=patient, to_discharge=discharge)
