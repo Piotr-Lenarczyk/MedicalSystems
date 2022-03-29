7d00b28407364e2f8d5e477c672dc75ba04dbf37
Piotr-Lenarczyk <piotr.lenarczyk@op.pl>
2022-03-29 19:55:52 +0200

    Migrate back to SQLite

--- config/settings.py
+++ config/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django',
-        'USER': 'admin',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
